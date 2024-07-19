3213c6d5349db3896d4669f7763f6adc149a567b
muhammed770 <mail.muhammed2002@gmail.com>
2024-07-19 18:55:04 +0530

    max age added

--- api/crab.rs
+++ api/crab.rs
@@ -10,6 +10,10 @@ pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
+        .header("Cache-control",format!(
+                "public, max-age=0, must-revalidate, s-maxage={s_maxage}",
+                s_maxage = 1 * 60 * 60
+            ),)
         .body(
             json!({ "message": "crab is the best!" }).to_string()
             .into(),
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    // async redirects() {
+    //     if (process.env.NODE_ENV === "development") {
+    //         return [
+    //             {
+    //                 source: '/api/:slug',
+    //                 destination: 'http:0.0.0.0:3001/api/:slug',
+    //                 permanent: true
+    //             }
+    //         ]
+    //     }
+    // }
+};
 
 export default nextConfig;
